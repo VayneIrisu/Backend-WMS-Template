4547503b4e27557cb754a6b56206e9d1d3731160
unknown <vayneirisu@gmail.com>
2026-05-10 20:57:37 +0700

    updte frontend link

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'https://frontend-wms-template.vercel.app',
   credentials: true,
 }));
 app.use(express.json({ limit: '10mb' }));
